e69148c605ae9ce2c7ef0917282d80445d63d637
atrevisanut <atrevisanut@gmail.com>
2017-05-11 11:55:28 -0600

    added div

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: 3001,
-  MONGO_URI: 'mongodb://localhost:27017/buddies', //Change db name from app to whatever your db name is
+  MONGO_URI: 'mongodb://mentors:ES6javascript@ds137801.mlab.com:37801/student-pair-buddies', //Change db name from app to whatever your db name is
   SESSION_SECRET: 'gweriwrb-erfawrg45-oasWsd', // Change your secret
 };
