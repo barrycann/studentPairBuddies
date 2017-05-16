975d7764277a61a059d9a2c1fc89376a01859a38
Stephen Brinkworth <xoman5144@gmail.com>
2017-05-16 11:03:50 -0600

    finished student page

--- server/models/cohort.js
+++ server/models/cohort.js
@@ -5,7 +5,8 @@ var cohort = new mongoose.Schema({
   title: { type: String },
   last_update: { type: Date },
   pairs: [[{type: objectId, ref: 'user'}]],
-  slack_channel: {type: String}
+  slack_channel: {type: String},
+  notify: {type: Boolean, default: true}
 });
 
 module.exports = mongoose.model('cohort', cohort);
